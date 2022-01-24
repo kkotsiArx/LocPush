e0dffd14daab893b5c3f0e41244923b2c28ededf
Kristo <kkotsi@arx.net>
2022-01-24 16:12:04 +0200

    remove unused targets

--- Package.swift
+++ Package.swift
@@ -19,15 +19,9 @@ let package = Package(
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "LocPush",
-            dependencies: []),
         .binaryTarget(
             name: "LocPushBinary",
             path: "binary/LocPush.xcframework"
         ),
-        .testTarget(
-            name: "LocPushTests",
-            dependencies: ["LocPush"]),
     ]
 )
